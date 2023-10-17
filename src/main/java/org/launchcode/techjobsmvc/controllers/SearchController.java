1b0c4cc2a22af7eb5dcf6df6779fc66c1c254bac
Ronald Gutierrez <ronaldgutierrezj@gmail.com>
2023-10-16 21:53:39 -0500

    task 3 completed, tess3 passed

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,12 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -23,5 +26,14 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
-}
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam(required = false) String searchType, @RequestParam(required = false)String searchTerm) {
 
+        String searchTitle = "Jobs With " + columnChoices.get(searchType) + ": " + searchTerm;
+
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("title",searchTitle);
+        model.addAttribute("jobs", JobData.findByColumnAndValue(searchType, searchTerm));
+        return "search";
+    }
+}
