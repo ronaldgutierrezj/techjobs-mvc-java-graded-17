4b25b99d2542685a68e4836009a2950db729e6e6
Ronald Gutierrez <ronaldgutierrezj@gmail.com>
2023-10-19 23:27:01 -0500

    super bonus mission parts:1,2,3 completed

--- src/main/java/org/launchcode/techjobsmvc/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/HomeController.java
@@ -3,6 +3,7 @@ package org.launchcode.techjobsmvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.HashMap;
 
@@ -10,16 +11,16 @@ import java.util.HashMap;
  * Created by LaunchCode
  */
 @Controller
-public class HomeController {
+public class HomeController  extends TechJobsController{
 
     @GetMapping(value = "/")
-    public String index(Model model) {
+    public String index () {
 
-        HashMap<String, String> actionChoices = new HashMap<>();
-        actionChoices.put("search", "Search");
-        actionChoices.put("list", "List");
+//        HashMap<String, String> actionChoices = new HashMap<>();
+//        actionChoices.put("search", "Search");
+//        actionChoices.put("list", "List");
 
-        model.addAttribute("actions", actionChoices);
+//        model.addAttribute("actions", TechJobsController.getActionChoices());
 
         return "index";
     }
